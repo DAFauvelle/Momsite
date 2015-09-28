624b260d48067a7fa8d60991d4cbf28693e387ee
David-Alexandre Fauvelle <dafauvelle@dafauvelle.local>
2015-09-28 14:20:23 -0400

    tempo model change

--- articles/models.py
+++ articles/models.py
@@ -11,24 +11,24 @@ class Family(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length= 100)
     text = RichTextField()
-    family = models.ForeignKey(Family) 
+    family = models.ForeignKey(Family)
     family_name = family.name
-    parent = models.ForeignKey('self')
+    #parent = models.ForeignKey('self')
 
     def __unicode__(self):
         return self.title
-        
+
 class Reference(models.Model):
     title = models.CharField(max_length=255)
     article = models.ForeignKey(Article)
-    
+
     def __unicode__(self):
         return self.title
-        
+
 class Liens(models.Model):
     title = models.CharField(max_length=255)
     addresse = models.URLField(max_length=255)
-    
+
     def __unicode__(self):
         return self.title
 
@@ -36,15 +36,14 @@ class Photos(models.Model):
     title = models.CharField(max_length=255)
     la_photo = models.ImageField(upload_to = "images/", max_length = 255)
     article = models.ForeignKey(Article)
-    
+
     def __unicode__(self):
         return self.title
-        
+
 class ImagePDF(models.Model):
     title = models.CharField(max_length=255)
     la_photo = models.FileField(upload_to = "images/", max_length = 255)
     article = models.ForeignKey(Article)
-    
+
     def __unicode__(self):
         return self.title
-    
